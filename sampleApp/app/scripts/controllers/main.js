d3416b655404ac0ee1fed2973ea3ca50cb7d7d84
Srikanth Chenna <srikanthchenna543@gmai.c.om>
2014-05-04 20:21:06 +0530

    sample

---
+++ sampleApp/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('sampleAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
